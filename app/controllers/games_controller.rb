77bedff909401fab651a839ec5e148d62345617c
Shona Chan <mojavealoe@gmail.com>
2023-03-06 13:20:14 +0800

    adds games controller, routes to new and score views

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,21 @@
+class GamesController < ApplicationController
+  def new
+
+  end
+
+  def score
+
+  end
+end
+
+
+# /new (new action)
+# displays grid letters
+# form for user to type the word -> button to submit
+# submit to /score
+
+
+# /score (score action)
+# receives this form info in params
+# compute the user score
+# display the score
